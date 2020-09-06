ef4d628b8f421d2e52da8d8ff28c15b2a6f09d46
xiao luo <xiao.luo@thoughtworks.com>
2020-09-06 17:46:05 +0800

    [luoxiao] feat:Complete the fourth JavaScript exercise

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,25 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  var Node = function (value) {
+    this.value = value;
+    this.next = null;
+  }
+  var length = 0;
+  var head = null;
+  if (undefined == array || array == null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length == 0) {
+    throw new Error('Creating list from empty array');
+  }
+  var curNode = new Node(array[0]);
+  head = curNode;
+  var arraySize = array.length;
+  for(let i = 1; i < arraySize; i++){
+    var node = new Node(array[i]);
+    curNode.next = node;
+    curNode = node;
+  }
+  return head;
 }
