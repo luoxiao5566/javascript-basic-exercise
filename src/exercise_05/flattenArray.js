9c62d4f4f19576021c2c6427001fae39e32982f9
xiao luo <xiao.luo@thoughtworks.com>
2020-09-06 18:10:48 +0800

    [luoxiao] feat:Complete the fifth JavaScript exercise

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,20 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (undefined == array || array == null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  var arraySize = array.length;
+  var flattenResult = [];
+  for(let i = 0; i < arraySize; i++){
+    let temp = array[i].length;
+    if (undefined == temp){
+      flattenResult.push(array[i]);
+      continue;
+    }
+    for(let j = 0; j < temp; j++){
+      flattenResult.push(array[i][j])
+    }
+  }
+  return flattenResult;
 }
